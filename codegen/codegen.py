975bf1d5d04cee4761f9e3d432ecf11f3d150ce4
pmtri80 <pmtri80@6a0925fc-cb2e-11dd-86c2-0f8696b7b6f9>
2009-10-05 09:16:58 +0000

    created function_transformers.py

--- codegen/codegen.py
+++ codegen/codegen.py
@@ -2,7 +2,7 @@
 import os
 from pygccxml import declarations
 from pyplusplus import module_builder, messages
-from pyplusplus import function_transformers as FT
+import function_transformers as FT
 from pyplusplus.module_builder import call_policies as CP
 
 #Creating an instance of class that will help you to expose your declarations
@@ -73,138 +73,6 @@ CV_VERSION          = "2.0.0"
 mb.decls().exclude()
 
 
-def expose_addressof_member(klass, member_name, exclude_member=True):
-    klass.include_files.append( "boost/python/long.hpp" )
-    if exclude_member:
-        klass.var(member_name).exclude()
-    klass.add_declaration_code('''
-    boost::python::long_ get_MEMBER_NAME_addr( CLASS_TYPE const & inst ){
-        return boost::python::long_((int)&inst.MEMBER_NAME);
-    }
-    '''.replace("MEMBER_NAME", member_name).replace("CLASS_TYPE", klass.decl_string))
-    klass.add_registration_code('def( "get_MEMBER_NAME_addr", &::get_MEMBER_NAME_addr )'.replace("MEMBER_NAME", member_name))
-    # TODO: finish the wrapping with ctypes code
-    
-def expose_member_as_str(klass, member_name):
-    klass.include_files.append( "boost/python/object.hpp" )
-    klass.include_files.append( "boost/python/str.hpp" )
-    klass.var(member_name).exclude()
-    klass.add_wrapper_code('''
-    static bp::object get_MEMBER_NAME( CLASS_TYPE const & inst ){        
-        return inst.MEMBER_NAME? bp::str(inst.MEMBER_NAME): bp::object();
-    }
-    '''.replace("MEMBER_NAME", member_name).replace("CLASS_TYPE", klass.decl_string))
-    klass.add_registration_code('''
-    add_property( "MEMBER_NAME", bp::make_function(&CLASS_TYPE_wrapper::get_MEMBER_NAME) )
-    '''.replace("MEMBER_NAME", member_name).replace("CLASS_TYPE", klass.decl_string))
-    
-
-def remove_ptr( type_ ):
-    if declarations.is_pointer( type_ ):
-        return declarations.remove_pointer( type_ )
-    else:
-        raise TypeError( 'Type should be a pointer, got %s.' % type_ )
-
-
-# -----------------------------------------------------------------------------------------------
-# Function transfomers
-# -----------------------------------------------------------------------------------------------
-
-# input_double_pointee_t
-class input_double_pointee_t(FT.transformer_t):
-    """Handles a double pointee input.
-    
-    Convert by dereferencing: do_smth(your_type **v) -> do_smth(your_type v)
-
-    Right now compiler should be able to use implicit conversion
-    """
-
-    def __init__(self, function, arg_ref):
-        FT.transformer.transformer_t.__init__( self, function )
-        self.arg = self.get_argument( arg_ref )
-        self.arg_index = self.function.arguments.index( self.arg )
-        if not declarations.is_pointer( self.arg.type ):
-            raise ValueError( '%s\nin order to use "input_double_pointee_t" transformation, argument %s type must be a pointer or a array (got %s).' ) \
-                  % ( function, self.arg_ref.name, arg.type)
-
-    def __str__(self):
-        return "input_double_pointee(%s)" % self.arg.name
-
-    def __configure_sealed( self, controller ):
-        w_arg = controller.find_wrapper_arg( self.arg.name )
-        tmp_type = remove_ptr( self.arg.type )
-        w_arg.type = remove_ptr( tmp_type )
-        if not declarations.is_convertible( w_arg.type, self.arg.type ):
-            controller.add_pre_call_code("%s tmp_%s = reinterpret_cast< %s >(& %s);" % ( tmp_type, w_arg.name, tmp_type, w_arg.name ))
-            casting_code = 'reinterpret_cast< %s >( & tmp_%s )' % (self.arg.type, w_arg.name)
-            controller.modify_arg_expression(self.arg_index, casting_code)
-
-    def __configure_v_mem_fun_default( self, controller ):
-        self.__configure_sealed( controller )
-
-    def configure_mem_fun( self, controller ):
-        self.__configure_sealed( controller )
-
-    def configure_free_fun(self, controller ):
-        self.__configure_sealed( controller )
-
-    def configure_virtual_mem_fun( self, controller ):
-        self.__configure_v_mem_fun_default( controller.default_controller )
-
-    def required_headers( self ):
-        """Returns list of header files that transformer generated code depends on."""
-        return []
-
-def input_double_pointee( *args, **keywd ):
-    def creator( function ):
-        return input_double_pointee_t( function, *args, **keywd )
-    return creator
-
-# input_smart_pointee_t
-class input_smart_pointee_t(FT.transformer_t):
-    """Handles a pointee input.
-    
-    Convert by dereferencing: do_smth(your_type *v) -> do_smth(object v2)
-    where v2 is either of type NoneType or type 'your_type'. 
-    If v2 is None, v is NULL.  Otherwise, v is the pointer to v2.
-    """
-
-    def __init__(self, function, arg_ref):
-        FT.transformer_t.__init__( self, function )
-        self.arg = self.get_argument( arg_ref )
-        self.arg_index = self.function.arguments.index( self.arg )
-
-    def __str__(self):
-        return "input_smart_pointee(%s)" % self.arg.name
-
-    def required_headers( self ):
-        """Returns list of header files that transformer generated code depends on."""
-        return [ "boost/python/object.hpp", "boost/python/extract.hpp" ]
-
-    def __configure_sealed( self, controller ):
-        w_arg = controller.find_wrapper_arg( self.arg.name )
-        data_type = declarations.remove_const(remove_ptr( self.arg.type ))
-        w_arg.type = declarations.dummy_type_t( "boost::python::object" )
-        controller.add_pre_call_code("%s const &tmp_%s = bp::extract<%s const &>(%s);" % (data_type, w_arg.name, data_type, w_arg.name))
-        controller.modify_arg_expression(self.arg_index, "(%s.ptr() != Py_None)? (%s)(&tmp_%s): 0" % (w_arg.name, self.arg.type, w_arg.name))
-
-    def __configure_v_mem_fun_default( self, controller ):
-        self.__configure_sealed( controller )
-
-    def configure_mem_fun( self, controller ):
-        self.__configure_sealed( controller )
-
-    def configure_free_fun(self, controller ):
-        self.__configure_sealed( controller )
-
-    def configure_virtual_mem_fun( self, controller ):
-        self.__configure_v_mem_fun_default( controller.default_controller )
-
-def input_smart_pointee( *args, **keywd ):
-    def creator( function ):
-        return input_smart_pointee_t( function, *args, **keywd )
-    return creator
-    
 # initialize list of transformer creators for each free function
 for z in mb.free_funs():
     z._transformer_creators = []
@@ -214,62 +82,14 @@ for z in mb.free_funs():
     for arg in z.arguments:
         if declarations.is_pointer(arg.type) and not declarations.is_pointer(declarations.remove_pointer(arg.type)) and \
             (arg.type.decl_string.startswith('::Cv') or arg.type.decl_string.startswith('::_Ipl')):
-            z._transformer_creators.append(input_smart_pointee(arg.name))
+            z._transformer_creators.append(FT.input_smart_pointee(arg.name))
             if arg.default_value == '0' or arg.default_value == 'NULL':
                 arg.default_value = 'bp::object()'
 # for z in mb.mem_funs(): # TODO: fix
     # for i in xrange(z.arguments):
         # if declarations.is_pointer(z.arguments[i]):
-            # z._transformer_creators.append(input_smart_pointee(i+1))
+            # z._transformer_creators.append(FT.input_smart_pointee(i+1))
 
-
-# input_string_t
-class input_string_t(FT.transformer_t):
-    """Handles a string.
-    
-    Convert: do_smth(void *v) -> do_smth(str v2)
-    where v2 is a Python string and v is a pointer to its content.
-    If vs is None, then v is NULL.
-    """
-
-    def __init__(self, function, arg_ref):
-        FT.transformer_t.__init__( self, function )
-        self.arg = self.get_argument( arg_ref )
-        self.arg_index = self.function.arguments.index( self.arg )
-        if not declarations.is_pointer( self.arg.type ):
-            raise ValueError( '%s\nin order to use "input_string_t" transformation, argument %s type must be a pointer (got %s).' ) \
-                  % ( function, arg_ref, self.arg.type)
-
-    def __str__(self):
-        return "input_string(%s)" % self.arg.name
-
-    def required_headers( self ):
-        """Returns list of header files that transformer generated code depends on."""
-        return [ "boost/python/str.hpp", "boost/python/object.hpp", "boost/python/extract.hpp" ]
-
-    def __configure_sealed( self, controller ):
-        w_arg = controller.find_wrapper_arg( self.arg.name )
-        w_arg.type = declarations.dummy_type_t( "boost::python::object" )
-        if self.arg.default_value == '0' or self.arg.default_value == 'NULL':
-            w_arg.default_value = 'bp::object()'
-        controller.modify_arg_expression(self.arg_index, "(%s.ptr() != Py_None)? (void *)((const char *)bp::extract<const char *>(static_cast<bp::str>(%s))): 0" % (w_arg.name, w_arg.name))
-
-    def __configure_v_mem_fun_default( self, controller ):
-        self.__configure_sealed( controller )
-
-    def configure_mem_fun( self, controller ):
-        self.__configure_sealed( controller )
-
-    def configure_free_fun(self, controller ):
-        self.__configure_sealed( controller )
-
-    def configure_virtual_mem_fun( self, controller ):
-        self.__configure_v_mem_fun_default( controller.default_controller )
-
-def input_string( *args, **keywd ):
-    def creator( function ):
-        return input_string_t( function, *args, **keywd )
-    return creator
 
 
             
@@ -415,7 +235,7 @@ cvmat.include_files.append( "boost/python/object.hpp" )
 cvmat.include_files.append( "boost/python/str.hpp" )
 cvmat.add_wrapper_code('''
 static bp::object get_data( CvMat const & inst ){        
-    return bp::str((const char *)inst.data.ptr, (inst.step? inst.step: CV_ELEM_SIZE(inst.type)*inst.cols)*inst.rows);
+    return inst.data.ptr? bp::str((const char *)inst.data.ptr, (inst.step? inst.step: CV_ELEM_SIZE(inst.type)*inst.cols)*inst.rows) : bp::object();
 }
 ''')
 cvmat.add_registration_code('''
@@ -447,7 +267,7 @@ cvmatnd.include_files.append( "boost/python/object.hpp" )
 cvmatnd.include_files.append( "boost/python/str.hpp" )
 cvmatnd.add_wrapper_code('''
 static bp::object get_data( CvMatND const & inst ){        
-    return bp::str((const char *)inst.data.ptr, inst.dim[0].step*inst.dim[0].size);
+    return inst.data.ptr? bp::str((const char *)inst.data.ptr, inst.dim[0].step*inst.dim[0].size): bp::object();
 }
 ''')
 cvmatnd.add_registration_code('''
@@ -531,7 +351,7 @@ iplimage.include_files.append( "boost/python/object.hpp" )
 iplimage.include_files.append( "boost/python/str.hpp" )
 iplimage.add_wrapper_code('''
 static bp::object get_data( _IplImage const & inst ){        
-    return bp::str(inst.imageData, inst.imageSize);
+    return inst.imageData? bp::str(inst.imageData, inst.imageSize) : bp::object();
 }
 ''')
 iplimage.add_registration_code('''
@@ -584,13 +404,19 @@ for z in mb.free_funs(lambda decl: decl.name.startswith('cvClone')):
 for z in mb.free_funs(lambda decl: decl.name.startswith('cvRelease')):
     if not z.name in ('cvRelease', 'cvReleaseMemStorage', 'cvReleaseData', 'cvReleaseFileStorage'): # TODO: fix
         add_underscore(z)
-        z._transformer_creators.append(input_double_pointee(0))
+        z._transformer_creators.append(FT.input_double_pointee(0))
         
 # cvInit... functions
 for z in mb.free_funs(lambda decl: decl.name.startswith('cvInit')):
     add_underscore(z)
     z.call_policies = CP.return_self()
 
+# function argument 'void *data'
+for f in mb.free_funs():
+    for arg in f.arguments:
+        if arg.name == 'data' and 'void *' in arg.type.decl_string:
+            f._transformer_creators.append(FT.input_string(arg.name))
+            break
 
 # cvCreateImage
 cc.write('''
@@ -682,6 +508,38 @@ def cvCreateMat(rows, cols, cvmat_type):
 
 ''')
 
+cc.write('''
+# Minh-Tri's helpers
+def cvCreateMatFromCvPoint2D32fList(points):
+    """CvMat cvCreateMatFromCvPoint2D32fList(list_or_tuple_of_CvPoint2D32f points)
+    
+    Creates a new matrix from a list/tuple of CvPoint2D32f points
+    """
+    cols = len(points)
+    z = cvCreateMat(1, cols, CV_32FC2)
+    for i in range(cols):
+        x = points[i]
+        y = z[0,i]
+        y[0] = x.x
+        y[1] = x.y
+    return z
+
+def cvCreateMatFromCvPointList(points):
+    """CvMat cvCreateMatFromCvPointList(list_or_tuple_of_CvPoint points)
+    
+    Creates a new matrix from a list/tuple of CvPoint points
+    """
+    cols = len(points)
+    z = cvCreateMat(1, cols, CV_32SC2)
+    for i in range(cols):
+        x = points[i]
+        y = z[0,i]
+        y[0] = x.x
+        y[1] = x.y
+    return z
+
+''')
+
 # cvCreateMatHeader
 cc.write('''
 def cvCreateMatHeader(rows, cols, cvmat_type):
@@ -700,11 +558,9 @@ CV_AUTOSTEP = 0x7fffffff
 
 
 # cvInitMatHeader
-z = mb.free_fun('cvInitMatHeader')
-z._transformer_creators.append(input_string('data'))
 cc.write('''
 def cvInitMatHeader(mat, rows, cols, cvmat_type, data=None, step=CV_AUTOSTEP):
-    """CvMat cvInitMatHeader(CvMat mat, int rows, int cols, int type, void* data=NULL, int step=CV_AUTOSTEP)
+    """CvMat cvInitMatHeader(CvMat mat, int rows, int cols, int type, string data=None, int step=CV_AUTOSTEP)
 
     Initializes matrix header
     """
@@ -715,6 +571,23 @@ def cvInitMatHeader(mat, rows, cols, cvmat_type, data=None, step=CV_AUTOSTEP):
     
 ''')
 
+
+# cvMat
+mb.free_fun('cvMat').include()
+
+# cvCloneMat
+cc.write('''
+def cvCloneMat(mat):
+    """CvMat cvCloneMat(const CvMat mat)
+
+    Creates matrix copy
+    """
+    z = _PE._cvCloneMat(mat)
+    if z is not None:
+        z._owner = True
+    return z
+
+''')
 
 # cvReleaseData
 add_underscore(mb.free_fun('cvReleaseData'))
