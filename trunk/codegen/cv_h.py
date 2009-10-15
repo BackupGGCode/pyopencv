abae6c12386510feaf77321a2432c1315c4e3db1
pmtri80 <pmtri80@6a0925fc-cb2e-11dd-86c2-0f8696b7b6f9>
2009-10-15 17:19:10 +0000

    a lot of changes

--- trunk/codegen/cv_h.py
+++ trunk/codegen/cv_h.py
@@ -38,16 +38,152 @@ CV_GAUSSIAN = 2
 CV_MEDIAN = 3
 CV_BILATERAL = 4
 
+CV_INPAINT_NS = 0
+CV_INPAINT_TELEA = 1
+
+CV_SCHARR = -1
+CV_MAX_SOBEL_KSIZE = 7
+
+CV_BGR2BGRA =   0
+CV_RGB2RGBA =   CV_BGR2BGRA
+
+CV_BGRA2BGR =   1
+CV_RGBA2RGB =   CV_BGRA2BGR
+
+CV_BGR2RGBA =   2
+CV_RGB2BGRA =   CV_BGR2RGBA
+
+CV_RGBA2BGR =   3
+CV_BGRA2RGB =   CV_RGBA2BGR
+
+CV_BGR2RGB  =   4
+CV_RGB2BGR  =   CV_BGR2RGB
+
+CV_BGRA2RGBA =  5
+CV_RGBA2BGRA =  CV_BGRA2RGBA
+
+CV_BGR2GRAY =   6
+CV_RGB2GRAY =   7
+CV_GRAY2BGR =   8
+CV_GRAY2RGB =   CV_GRAY2BGR
+CV_GRAY2BGRA =  9
+CV_GRAY2RGBA =  CV_GRAY2BGRA
+CV_BGRA2GRAY =  10
+CV_RGBA2GRAY =  11
+
+CV_BGR2BGR565 = 12
+CV_RGB2BGR565 = 13
+CV_BGR5652BGR = 14
+CV_BGR5652RGB = 15
+CV_BGRA2BGR565 = 16
+CV_RGBA2BGR565 = 17
+CV_BGR5652BGRA = 18
+CV_BGR5652RGBA = 19
+
+CV_GRAY2BGR565 = 20
+CV_BGR5652GRAY = 21
+
+CV_BGR2BGR555  = 22
+CV_RGB2BGR555  = 23
+CV_BGR5552BGR  = 24
+CV_BGR5552RGB  = 25
+CV_BGRA2BGR555 = 26
+CV_RGBA2BGR555 = 27
+CV_BGR5552BGRA = 28
+CV_BGR5552RGBA = 29
+
+CV_GRAY2BGR555 = 30
+CV_BGR5552GRAY = 31
+
+CV_BGR2XYZ =    32
+CV_RGB2XYZ =    33
+CV_XYZ2BGR =    34
+CV_XYZ2RGB =    35
+
+CV_BGR2YCrCb =  36
+CV_RGB2YCrCb =  37
+CV_YCrCb2BGR =  38
+CV_YCrCb2RGB =  39
+
+CV_BGR2HSV =    40
+CV_RGB2HSV =    41
+
+CV_BGR2Lab =    44
+CV_RGB2Lab =    45
+
+CV_BayerBG2BGR = 46
+CV_BayerGB2BGR = 47
+CV_BayerRG2BGR = 48
+CV_BayerGR2BGR = 49
+
+CV_BayerBG2RGB = CV_BayerRG2BGR
+CV_BayerGB2RGB = CV_BayerGR2BGR
+CV_BayerRG2RGB = CV_BayerBG2BGR
+CV_BayerGR2RGB = CV_BayerGB2BGR
+
+CV_BGR2Luv =    50
+CV_RGB2Luv =    51
+CV_BGR2HLS =    52
+CV_RGB2HLS =    53
+
+CV_HSV2BGR =    54
+CV_HSV2RGB =    55
+
+CV_Lab2BGR =    56
+CV_Lab2RGB =    57
+CV_Luv2BGR =    58
+CV_Luv2RGB =    59
+CV_HLS2BGR =    60
+CV_HLS2RGB =    61
+
+CV_COLORCVT_MAX = 100
+
+CV_INTER_NN     = 0
+CV_INTER_LINEAR = 1
+CV_INTER_CUBIC  = 2
+CV_INTER_AREA = 3
+
+CV_WARP_FILL_OUTLIERS = 8
+CV_WARP_INVERSE_MAP = 16
+
+CV_SHAPE_RECT = 0
+CV_SHAPE_CROSS = 1
+CV_SHAPE_ELLIPSE = 2
+CV_SHAPE_CUSTOM = 100
+
+CV_MOP_OPEN = 2
+CV_MOP_CLOSE = 3
+CV_MOP_GRADIENT = 4
+CV_MOP_TOPHAT = 5
+CV_MOP_BLACKHAT = 6
+
+
 
     ''')
 
     # functions
     # for z in (
         # 'cvCopyMakeBorder', 'cvSmooth', 'cvFilter2D', 'cvIntegral', 'cvPyrDown', 'cvPyrUp',
+        # 'cvPyrMeanShiftFiltering', 'cvWatershed', 'cvInpaint', 'cvSobel', 'cvLaplace',
+        # 'cvCvtColor', 'cvResize', 'cvWarpAffine', 'cvWarpPerspective',
+        # 'cvRemap', 'cvConvertMaps', 'cvLogPolar', 'cvLinearPolar',
+        # 'cvErode', 'cvDilate', 'cvMorphologyEx', 
+        # 'cvMoments', 'cvGetSpatialMoment', 'cvGetCentralMoment', 'cvGetNormalizedCentralMoment', 'cvGetHuMoments',
+        
         # ):
         # mb.free_fun(z).include()
         
     # TODO: fix these functions:
+    # cvCreatePyramid, cvReleasePyramid, cvPyrSegmentation
+    # cvCreateStructuringElementEx, cvReleaseStructuringElement
 
+    # cvGetAffineTransform
+    # FT.expose_func(mb.free_fun('cvGetAffineTransform'), return_arg_index=3)
 
+    # cv2DRotationMatrix
+    # FT.expose_func(mb.free_fun('cv2DRotationMatrix'), return_arg_index=4)
+    
+    # cvGetPerspectiveTransform
+    # FT.expose_func(mb.free_fun('cvGetPerspectiveTransform'), return_arg_index=3)
 
+    
