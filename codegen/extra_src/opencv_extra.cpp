d9f67a546cc989563672ba3d377ffbe2565c0edf
pmtri80 <pmtri80@6a0925fc-cb2e-11dd-86c2-0f8696b7b6f9>
2009-10-20 14:38:45 +0000

    finished highgui.h/cvCreateTrackbar*

--- codegen/extra_src/opencv_extra.cpp
+++ codegen/extra_src/opencv_extra.cpp
@@ -7,8 +7,17 @@
 
 namespace bp = boost::python;
 
+void CV_CDECL sdTrackbarCallback2(int pos, void* userdata)
+{
+    bp::object items(bp::handle<>(bp::borrowed((PyObject *)userdata)));
+    (items[0])(pos, bp::object(items[1])); // need a copy of items[1] to make it safe with threading
+}
+
+
 void CV_CDECL sdMouseCallback(int event, int x, int y, int flags, void* param)
 {
     bp::object items(bp::handle<>(bp::borrowed((PyObject *)param)));
     (items[0])(event, x, y, flags, bp::object(items[1])); // need a copy of items[1] to make it safe with threading
 }
+
+
