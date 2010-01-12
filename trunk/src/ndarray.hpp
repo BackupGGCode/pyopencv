4e80610b0e9dd8074b6cdbd4991f9fc9c248adab
pmtri80 <pmtri80@6a0925fc-cb2e-11dd-86c2-0f8696b7b6f9>
2010-01-12 09:30:18 +0000

    added generated source code for pyopencv

---
+++ trunk/src/ndarray.hpp
@@ -0,0 +1,200 @@
+// Copyright David Abrahams 2002.
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+#ifndef SD_NDARRAY_HPP
+# define SD_NDARRAY_HPP
+
+# include <boost/python/detail/prefix.hpp>
+
+# include <boost/python/tuple.hpp>
+# include <boost/python/str.hpp>
+# include <boost/preprocessor/iteration/local.hpp>
+# include <boost/preprocessor/cat.hpp>
+# include <boost/preprocessor/repetition/enum.hpp>
+# include <boost/preprocessor/repetition/enum_params.hpp>
+# include <boost/preprocessor/repetition/enum_binary_params.hpp>
+
+#include "opencv_headers.hpp"
+
+namespace boost { namespace python {
+
+namespace aux
+{
+    struct array_object_manager_traits
+    {
+        static bool check(PyObject* obj);
+        static detail::new_non_null_reference adopt(PyObject* obj);
+        static PyTypeObject const* get_pytype() ;
+    };
+} // namespace aux
+
+struct ndarray : object
+{
+public:
+    ndarray() : object() {}
+
+    void check() const;
+
+    int ndim() const;
+    const int *shape() const;
+    const int *strides() const;
+    int itemsize() const;
+    int dtype() const;
+    const void *data() const;
+    const void *getptr1(int i1) const;
+    const void *getptr2(int i1, int i2) const;
+    const void *getptr3(int i1, int i2, int i3) const;
+    bool iscontiguous() const;
+    
+    bool last_dim_as_cvchannel() const;
+    int cvrank() const; // = ndim() - last_dim_as_cvchannel()
+    
+public: // implementation detail - do not touch.
+    BOOST_PYTHON_FORWARD_OBJECT_CONSTRUCTORS(ndarray, object);
+};
+
+
+namespace converter
+{
+  template <>
+  struct object_manager_traits< ndarray >
+      : aux::array_object_manager_traits
+  {
+      BOOST_STATIC_CONSTANT(bool, is_specialized = true);
+  };
+}
+
+
+
+ndarray simplenew(int len, const int *shape, int dtype);
+
+// be careful with the new_() function. you must keep the data alive until the ndarray is deleted.
+ndarray new_(int len, const int *shape, int dtype, const int *strides, void *data, int flags);
+
+
+// ================================================================================================
+
+// dtypeof
+template<typename T>
+int dtypeof()
+{
+    char s[300];
+    sprintf( s, "Instantiation of function dtypeof() for class '%s' is not yet implemented.", typeid(T).name() );
+    PyErr_SetString(PyExc_NotImplementedError, s);
+    throw error_already_set(); 
+}
+
+template<> int dtypeof<char>();
+template<> int dtypeof<unsigned char>();
+template<> int dtypeof<short>();
+template<> int dtypeof<unsigned short>();
+template<> int dtypeof<long>();
+template<> int dtypeof<unsigned long>();
+template<> int dtypeof<int>();
+template<> int dtypeof<unsigned int>();
+template<> int dtypeof<float>();
+template<> int dtypeof<double>();
+
+// ================================================================================================
+
+// convert_ndarray
+template<typename T>
+void convert_ndarray( const ndarray &in_arr, T &out_arr )
+{
+    char s[300];
+    sprintf( s, "Instantiation of function convert_ndarray() for class '%s' is not yet implemented.", typeid(T).name() );
+    PyErr_SetString(PyExc_NotImplementedError, s);
+    throw error_already_set(); 
+}
+
+template<typename T>
+void convert_ndarray( const T &in_arr, ndarray &out_arr )
+{
+    char s[300];
+    sprintf( s, "Instantiation of function convert_ndarray() for class '%s' is not yet implemented.", typeid(T).name() );
+    PyErr_SetString(PyExc_NotImplementedError, s);
+    throw error_already_set(); 
+}
+
+// convert_ndarray, std::vector case
+// Note: because Python and C have different ways of allocating/reallocating memory,
+// it is UNSAFE to share data between ndarray and std::vector.
+// In this implementation, data is allocated and copied instead.
+template<typename T>
+void convert_ndarray( const ndarray &in_arr, std::vector<T> &out_arr )
+{
+    char s[100];
+    int nd = in_arr.ndim();
+    if(nd != 1)
+    {
+        sprintf(s, "Rank must be 1, rank=%d detected.", nd);
+        PyErr_SetString(PyExc_TypeError, s);
+        throw error_already_set(); 
+    }
+    if(in_arr.dtype() != dtypeof<T>())
+    {
+        sprintf(s, "Ndarray's element type is not the same as that of std::vector. ndarray's dtype=%d, vector's dtype=%d.", in_arr.dtype(), dtypeof<T>());
+        PyErr_SetString(PyExc_TypeError, s);
+        throw error_already_set(); 
+    }
+    
+    int len = in_arr.shape()[0];
+    
+    out_arr.resize(len);
+    for(int i = 0; i < len; ++i) out_arr[i] = *(T *)in_arr.getptr1(i);
+}
+
+extern template void convert_ndarray( const ndarray &in_arr, std::vector<char> &out_arr );
+extern template void convert_ndarray( const ndarray &in_arr, std::vector<unsigned char> &out_arr );
+extern template void convert_ndarray( const ndarray &in_arr, std::vector<short> &out_arr );
+extern template void convert_ndarray( const ndarray &in_arr, std::vector<unsigned short> &out_arr );
+extern template void convert_ndarray( const ndarray &in_arr, std::vector<long> &out_arr );
+extern template void convert_ndarray( const ndarray &in_arr, std::vector<unsigned long> &out_arr );
+extern template void convert_ndarray( const ndarray &in_arr, std::vector<int> &out_arr );
+extern template void convert_ndarray( const ndarray &in_arr, std::vector<unsigned int> &out_arr );
+extern template void convert_ndarray( const ndarray &in_arr, std::vector<float> &out_arr );
+extern template void convert_ndarray( const ndarray &in_arr, std::vector<double> &out_arr );
+
+// convert_ndarray, std::vector case
+template<typename T>
+void convert_ndarray( const std::vector<T> &in_arr, ndarray &out_arr )
+{
+    int len = in_arr.size();
+    out_arr = simplenew(1, &len, dtypeof<T>());
+    T *data = (T *)out_arr.data();
+    for(int i = 0; i < len; ++i) data[i] = in_arr[i];
+}
+
+extern template void convert_ndarray( const std::vector<char> &in_arr, ndarray &out_arr );
+extern template void convert_ndarray( const std::vector<unsigned char> &in_arr, ndarray &out_arr );
+extern template void convert_ndarray( const std::vector<short> &in_arr, ndarray &out_arr );
+extern template void convert_ndarray( const std::vector<unsigned short> &in_arr, ndarray &out_arr );
+extern template void convert_ndarray( const std::vector<long> &in_arr, ndarray &out_arr );
+extern template void convert_ndarray( const std::vector<unsigned long> &in_arr, ndarray &out_arr );
+extern template void convert_ndarray( const std::vector<int> &in_arr, ndarray &out_arr );
+extern template void convert_ndarray( const std::vector<unsigned int> &in_arr, ndarray &out_arr );
+extern template void convert_ndarray( const std::vector<float> &in_arr, ndarray &out_arr );
+extern template void convert_ndarray( const std::vector<double> &in_arr, ndarray &out_arr );
+
+
+ndarray as_ndarray(const object &obj);
+
+object as_Point2i(const ndarray &arr);
+object as_Point2f(const ndarray &arr);
+object as_Point2d(const ndarray &arr);
+
+object as_Point3i(const ndarray &arr);
+object as_Point3f(const ndarray &arr);
+object as_Point3d(const ndarray &arr);
+
+object as_Scalar(const ndarray &arr);
+object as_Mat(const ndarray &arr);
+object as_MatND(const ndarray &arr);
+
+void mixChannels(const tuple src, tuple dst, const ndarray &fromTo);
+tuple minMaxLoc(const object& a, const object& mask=object());
+
+}} // namespace boost::python
+
+#endif // SD_NDARRAY_HPP
---
+++ trunk/src/opencv_extra.cpp
@@ -0,0 +1,54 @@
+#include <boost/python/detail/prefix.hpp>
+#include <boost/python/extract.hpp>
+#include <boost/python/default_call_policies.hpp>
+#include <boost/python/object.hpp>
+
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+
+#include "opencv_extra.hpp"
+
+
+// ================================================================================================
+
+void CV_CDECL sdTrackbarCallback2(int pos, void* userdata)
+{
+    bp::object items(bp::handle<>(bp::borrowed((PyObject *)userdata)));
+    if(bp::object(items[0]).ptr() != Py_None) // invoke if not None
+        (items[0])(pos, bp::object(items[1])); // need a copy of items[1] to make it safe with threading
+}
+
+
+void CV_CDECL sdMouseCallback(int event, int x, int y, int flags, void* param)
+{
+    bp::object items(bp::handle<>(bp::borrowed((PyObject *)param)));
+    if(bp::object(items[0]).ptr() != Py_None) // invoke if not None
+        (items[0])(event, x, y, flags, bp::object(items[1])); // need a copy of items[1] to make it safe with threading
+}
+
+float CV_CDECL sdDistanceFunction( const float* a, const float*b, void* user_param )
+{
+    bp::object items(bp::handle<>(bp::borrowed((PyObject *)user_param)));
+    // pass 'a' and 'b' by address instead of by pointer
+    return bp::extract < float >((items[0])((int)a, (int)b, bp::object(items[1]))); // need a copy of items[1] to make it safe with threading
+}
+
+// ================================================================================================
+
+template void convert_Mat( const cv::Mat &in_arr, char *&out_arr );
+template void convert_Mat( const cv::Mat &in_arr, unsigned char *&out_arr );
+template void convert_Mat( const cv::Mat &in_arr, short *&out_arr );
+template void convert_Mat( const cv::Mat &in_arr, unsigned short *&out_arr );
+template void convert_Mat( const cv::Mat &in_arr, long *&out_arr );
+template void convert_Mat( const cv::Mat &in_arr, unsigned long *&out_arr );
+template void convert_Mat( const cv::Mat &in_arr, int *&out_arr );
+template void convert_Mat( const cv::Mat &in_arr, unsigned int *&out_arr );
+template void convert_Mat( const cv::Mat &in_arr, float *&out_arr );
+template void convert_Mat( const cv::Mat &in_arr, double *&out_arr );
+
+// ================================================================================================
+
+
