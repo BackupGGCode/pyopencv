b52bc682e840c01654616c2d0b0cc15116ce22bc
pmtri80 <pmtri80@6a0925fc-cb2e-11dd-86c2-0f8696b7b6f9>
2010-01-29 13:04:45 +0000

    changed version name

--- trunk/src/setup.py
+++ trunk/src/setup.py
@@ -110,7 +110,7 @@ bundled_files = find_libraries(C.opencv_runtime_library_dirs, C.opencv_runtime_l
 
 setup(
     name = "pyopencv",
-	version = '2.0.0.wr1.0.0',
+	version = '2.0.wr1.0.0',
 	description = DOCLINES[0],
 	author = 'Minh-Tri Pham',
 	author_email = 'pmtri80@gmail.com',
