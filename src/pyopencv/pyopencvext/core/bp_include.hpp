fe74f2f5700f9c33879942311ac401e8ebfc2ffb
pmtri80 <pmtri80@6a0925fc-cb2e-11dd-86c2-0f8696b7b6f9>
2010-02-09 02:11:04 +0000

    added bp_include.hpp

---
+++ src/pyopencv/pyopencvext/core/bp_include.hpp
@@ -0,0 +1,16 @@
+#ifndef BP_INClUDE_HPP
+#define BP_INClUDE_HPP
+
+namespace boost { namespace python {
+
+class object;
+class ndarray;
+class sequence;
+class list;
+class tuple;
+
+}}
+
+namespace bp = boost::python;
+
+#endif
