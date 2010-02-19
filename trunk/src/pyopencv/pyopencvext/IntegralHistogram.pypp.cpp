a153f3d2594ad217f95b11dacff250ec5c76f183
pmtri80 <pmtri80@6a0925fc-cb2e-11dd-86c2-0f8696b7b6f9>
2010-02-19 09:04:21 +0000

    renamed IntegralHistogram.hist() to IntegralHistogram.calcHist()

--- trunk/src/pyopencv/pyopencvext/IntegralHistogram.pypp.cpp
+++ trunk/src/pyopencv/pyopencvext/IntegralHistogram.pypp.cpp
@@ -5,29 +5,37 @@
 #include "opencv_extra.hpp"
 #include "__ctypes_integration.pypp.hpp"
 #include "opencv_headers.hpp"
+#include "opencv_extra.hpp"
 #include "IntegralHistogram.pypp.hpp"
 
 namespace bp = boost::python;
 
-static boost::python::object hist_45bfce131369d914df64f684506db8b4( ::sdopencv::IntegralHistogram & inst, ::cv::Rect rect ){
+static boost::python::object calcHist_7e977de6e27427b53a27b192cb2b370c( ::sdopencv::IntegralHistogram & inst, ::cv::Rect rect ){
     bp::sequence out_hist2;
     std::vector<int, std::allocator<int> > out_hist3;
-    inst.hist(rect, out_hist3);
+    inst.calcHist(rect, out_hist3);
     out_hist2 = convert_vector_to_seq(out_hist3);
     return bp::object( out_hist2 );
 }
 
+static boost::shared_ptr<sdopencv::IntegralHistogram> IntegralHistogram__init1__(int histSize, bp::sequence const &ranges, bool uniform)
+{
+    std::vector<float> ranges2; convert_seq_to_vector(ranges, ranges2);
+    return boost::shared_ptr<sdopencv::IntegralHistogram>(new sdopencv::IntegralHistogram(histSize, ranges2, uniform));
+}
+
 void register_IntegralHistogram_class(){
 
-    bp::class_< sdopencv::IntegralHistogram >( "IntegralHistogram", bp::init< int, std::vector< float > const &, bp::optional< bool > >(( bp::arg("histSize"), bp::arg("ranges"), bp::arg("uniform")=(bool)(true) )) )    
+    bp::class_< sdopencv::IntegralHistogram >( "IntegralHistogram", bp::no_init )    
         .add_property( "this", pyplus_conv::make_addressof_inst_getter< sdopencv::IntegralHistogram >() )    
         .def( 
-            "hist"
-            , (boost::python::object (*)( ::sdopencv::IntegralHistogram &,::cv::Rect ))( &hist_45bfce131369d914df64f684506db8b4 )
+            "calcHist"
+            , (boost::python::object (*)( ::sdopencv::IntegralHistogram &,::cv::Rect ))( &calcHist_7e977de6e27427b53a27b192cb2b370c )
             , ( bp::arg("inst"), bp::arg("rect") ) )    
         .def( 
             "__call__"
             , (void ( ::sdopencv::IntegralHistogram::* )( ::cv::Mat const & ) )( &::sdopencv::IntegralHistogram::operator() )
-            , ( bp::arg("image") ) );
+            , ( bp::arg("image") ) )    
+        .def("__init__", bp::make_constructor(&IntegralHistogram__init1__, bp::default_call_policies(), ( bp::arg("histSize"), bp::arg("ranges"), bp::arg("uniform")=bp::object(true) )));
 
 }
