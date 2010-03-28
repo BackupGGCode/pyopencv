6d1baec067dc8396ab5ba973177f1ddcceec7812
pmtri80 <pmtri80@6a0925fc-cb2e-11dd-86c2-0f8696b7b6f9>
2010-03-28 16:43:33 +0000

    impoved CvHaarClassifier

--- trunk/src/pyopencv/pyopencvext/CvHaarClassifier.pypp.cpp
+++ trunk/src/pyopencv/pyopencvext/CvHaarClassifier.pypp.cpp
@@ -14,28 +14,28 @@ static ::CvHaarFeature * get_haar_feature( ::CvHaarClassifier const & inst ) { r
 static cv::Mat get_threshold(::CvHaarClassifier const &inst)
 {
     cv::Mat threshold2;
-    convert_from_array_of_T_to_Mat(inst.threshold, inst.count, threshold2);
+    convert_from_array_of_T_to_Mat(inst.threshold, inst.count+0, threshold2);
     return threshold2;
 }
 
 static cv::Mat get_left(::CvHaarClassifier const &inst)
 {
     cv::Mat left2;
-    convert_from_array_of_T_to_Mat(inst.left, inst.count, left2);
+    convert_from_array_of_T_to_Mat(inst.left, inst.count+0, left2);
     return left2;
 }
 
 static cv::Mat get_right(::CvHaarClassifier const &inst)
 {
     cv::Mat right2;
-    convert_from_array_of_T_to_Mat(inst.right, inst.count, right2);
+    convert_from_array_of_T_to_Mat(inst.right, inst.count+0, right2);
     return right2;
 }
 
 static cv::Mat get_alpha(::CvHaarClassifier const &inst)
 {
     cv::Mat alpha2;
-    convert_from_array_of_T_to_Mat(inst.alpha, inst.count, alpha2);
+    convert_from_array_of_T_to_Mat(inst.alpha, inst.count+1, alpha2);
     return alpha2;
 }
 
@@ -43,6 +43,7 @@ void register_CvHaarClassifier_class(){
 
     bp::class_< CvHaarClassifier >( "CvHaarClassifier" )    
         .add_property( "this", pyplus_conv::make_addressof_inst_getter< CvHaarClassifier >() )    
+        .def_readwrite( "count", &CvHaarClassifier::count )    
         .add_property( "haar_feature", bp::make_function(&::get_haar_feature, bp::return_internal_reference<>()) )    
         .add_property( "threshold", &::get_threshold)    
         .add_property( "left", &::get_left)    
--- trunk/src/pyopencv/pyopencvext/CvHaarStageClassifier.pypp.cpp
+++ trunk/src/pyopencv/pyopencvext/CvHaarStageClassifier.pypp.cpp
@@ -4,21 +4,46 @@
 #include "__ctypes_integration.pypp.hpp"
 #include "opencv_headers.hpp"
 #include "boost/python/object.hpp"
+#include "boost/python/list.hpp"
+#include "boost/python/tuple.hpp"
 #include "CvHaarStageClassifier.pypp.hpp"
 
 namespace bp = boost::python;
 
-static ::CvHaarClassifier * get_classifier( ::CvHaarStageClassifier const & inst ) { return inst.classifier; }
+struct CvHaarStageClassifier_wrapper : CvHaarStageClassifier, bp::wrapper< CvHaarStageClassifier > {
+
+    CvHaarStageClassifier_wrapper(CvHaarStageClassifier const & arg )
+    : CvHaarStageClassifier( arg )
+      , bp::wrapper< CvHaarStageClassifier >(){
+        // copy constructor
+        
+    }
+
+    CvHaarStageClassifier_wrapper()
+    : CvHaarStageClassifier()
+      , bp::wrapper< CvHaarStageClassifier >(){
+        // null constructor
+        
+    }
+
+    static bp::object get_classifier( ::CvHaarStageClassifier const & inst ){
+        bp::list l;
+        for(int i = 0; i < inst.count; ++i)
+            l.append(inst.classifier[i]);
+        return bp::tuple(l);
+    }
+
+};
 
 void register_CvHaarStageClassifier_class(){
 
-    bp::class_< CvHaarStageClassifier >( "CvHaarStageClassifier" )    
+    bp::class_< CvHaarStageClassifier_wrapper >( "CvHaarStageClassifier" )    
         .add_property( "this", pyplus_conv::make_addressof_inst_getter< CvHaarStageClassifier >() )    
         .def_readwrite( "child", &CvHaarStageClassifier::child )    
         .def_readwrite( "count", &CvHaarStageClassifier::count )    
         .def_readwrite( "next", &CvHaarStageClassifier::next )    
         .def_readwrite( "parent", &CvHaarStageClassifier::parent )    
         .def_readwrite( "threshold", &CvHaarStageClassifier::threshold )    
-        .add_property( "classifier", bp::make_function(&::get_classifier, bp::return_internal_reference<>()) );
+        .add_property( "classifier", bp::make_function(&::CvHaarStageClassifier_wrapper::get_classifier) );
 
 }
