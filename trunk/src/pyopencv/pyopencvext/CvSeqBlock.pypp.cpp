9045a2a447a4fc7d37b6a47b8f5f1812937802a6
pmtri80 <pmtri80@6a0925fc-cb2e-11dd-86c2-0f8696b7b6f9>
2010-03-30 09:37:40 +0000

    improved exposal of member variables

--- trunk/src/pyopencv/pyopencvext/CvSeqBlock.pypp.cpp
+++ trunk/src/pyopencv/pyopencvext/CvSeqBlock.pypp.cpp
@@ -9,40 +9,22 @@
 
 namespace bp = boost::python;
 
-struct CvSeqBlock_wrapper : CvSeqBlock, bp::wrapper< CvSeqBlock > {
-
-    CvSeqBlock_wrapper(CvSeqBlock const & arg )
-    : CvSeqBlock( arg )
-      , bp::wrapper< CvSeqBlock >(){
-        // copy constructor
-        
-    }
-
-    CvSeqBlock_wrapper()
-    : CvSeqBlock()
-      , bp::wrapper< CvSeqBlock >(){
-        // null constructor
-        
-    }
-
-    static bp::object get_data( ::CvSeqBlock const & inst ){        
-        return inst.data? bp::str(inst.data): bp::object();
-    }
-
-};
-
 static ::CvSeqBlock * get_prev( ::CvSeqBlock const & inst ) { return inst.prev; }
 
 static ::CvSeqBlock * get_next( ::CvSeqBlock const & inst ) { return inst.next; }
 
+static bp::object get_data( ::CvSeqBlock const & inst ){        
+    return inst.data? bp::str(inst.data): bp::object();
+}
+
 void register_CvSeqBlock_class(){
 
-    bp::class_< CvSeqBlock_wrapper >( "CvSeqBlock" )    
+    bp::class_< CvSeqBlock >( "CvSeqBlock" )    
         .add_property( "this", pyplus_conv::make_addressof_inst_getter< CvSeqBlock >() )    
         .def_readwrite( "count", &CvSeqBlock::count )    
         .def_readwrite( "start_index", &CvSeqBlock::start_index )    
         .add_property( "prev", bp::make_function(&::get_prev, bp::return_internal_reference<>()) )    
         .add_property( "next", bp::make_function(&::get_next, bp::return_internal_reference<>()) )    
-        .add_property( "data", bp::make_function(&::CvSeqBlock_wrapper::get_data) );
+        .add_property( "data", &::get_data );
 
 }
