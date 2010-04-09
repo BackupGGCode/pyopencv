17b63ae70564a1c2af7abce4aadc04946b8820fc
pmtri80 <pmtri80@6a0925fc-cb2e-11dd-86c2-0f8696b7b6f9>
2010-04-09 12:29:34 +0000

    reenabled CvSlice to support old C-style structures

--- demo/convexhull.py
+++ demo/convexhull.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
                 my_random.randrange (0, image.cols / 2) + image.cols / 4)
 
         # compute the convex hull
-        hull = convexHull(points).to_list_of_Point2i()
+        hull = convexHull_int(points).to_list_of_Point2i()
         points = points.to_list_of_Point2i()
         
         # start with an empty image
