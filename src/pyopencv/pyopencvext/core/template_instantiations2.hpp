5f5c423a86eb89cd8f65162025a1a310433cbb17
pmtri80 <pmtri80@6a0925fc-cb2e-11dd-86c2-0f8696b7b6f9>
2010-04-22 13:50:03 +0000

    temporarily added template_instantiations2.hpp

---
+++ src/pyopencv/pyopencvext/core/template_instantiations2.hpp
@@ -0,0 +1,64 @@
+#ifndef SD_TEMPLATE_INSTANTIATIONS_H
+#define SD_TEMPLATE_INSTANTIATIONS_H
+
+struct dummy_struct3 {
+    struct dummy_struct2 {};
+    cv::Vec< unsigned char, 3 > var0;
+    cv::Point_< float > var1;
+    short unsigned int var2;
+    cv::Vec< float, 4 > var3;
+    std::vector< cv::Ptr< cv::Mat > > var4;
+    int var5;
+    float var6;
+    cv::Vec< float, 2 > var7;
+    char var8;
+    cv::Complex< double > var9;
+    cv::Vec< float, 3 > var10;
+    cv::RotatedRect var11;
+    cv::Point_< double > var12;
+    cv::Size_< int > var13;
+    cv::Vec< float, 6 > var14;
+    cv::Range var15;
+    uchar var16;
+    cv::FilterEngine var17;
+    cv::Ptr< cv::Mat > var18;
+    long var19;
+    cv::Vec< unsigned char, 4 > var20;
+    cv::Mat var21;
+    unsigned short var22;
+    cv::Point_< int > var23;
+    cv::Point3_< float > var24;
+    long long var25;
+    cv::Vec< unsigned char, 2 > var26;
+    cv::Vec< short, 4 > var27;
+    cv::Vec< short, 2 > var28;
+    cv::Vec< short, 3 > var29;
+    cv::Vec< double, 6 > var30;
+    cv::Ptr< cv::FilterEngine > var31;
+    unsigned char var32;
+    signed char var33;
+    cv::Vec< int, 4 > var34;
+    cv::Vec< int, 3 > var35;
+    unsigned long var36;
+    cv::Point3_< double > var37;
+    cv::Vec< double, 3 > var38;
+    cv::Point3_< int > var39;
+    cv::Vec< int, 2 > var40;
+    cv::Vec< unsigned short, 3 > var41;
+    unsigned int var42;
+    cv::Vec< double, 2 > var43;
+    cv::Vec< unsigned short, 4 > var44;
+    short var45;
+    cv::Scalar_< double > var46;
+    cv::Complex< float > var47;
+    double var48;
+    ushort var49;
+    schar var50;
+    cv::Rect_< int > var51;
+    cv::Size_< float > var52;
+    cv::Vec< double, 4 > var53;
+    unsigned long long var54;
+    cv::Vec< unsigned short, 2 > var55;
+};
+
+#endif
