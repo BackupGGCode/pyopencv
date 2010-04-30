fa07bfde17a8f596e20c9c3a60f7d2a64ee2c16e
pmtri80 <pmtri80@6a0925fc-cb2e-11dd-86c2-0f8696b7b6f9>
2010-04-30 14:05:28 +0000

    n/a

--- trunk/src_gen/cxtypes_h.py
+++ trunk/src_gen/cxtypes_h.py
@@ -243,9 +243,9 @@ CV_TYPE_NAME_SPARSE_MAT    = "opencv-sparse-matrix"
 # Other supplementary data type definitions
 #-----------------------------------------------------------------------------
 
-CV_TERMCRIT_ITER    = 1
-CV_TERMCRIT_NUMBER  = CV_TERMCRIT_ITER
-CV_TERMCRIT_EPS     = 2
+# CV_TERMCRIT_ITER    = 1
+# CV_TERMCRIT_NUMBER  = CV_TERMCRIT_ITER
+# CV_TERMCRIT_EPS     = 2
 
 CV_WHOLE_SEQ_END_INDEX = 0x3fffffff
 CV_WHOLE_SEQ = _PE.Range(0, CV_WHOLE_SEQ_END_INDEX)
