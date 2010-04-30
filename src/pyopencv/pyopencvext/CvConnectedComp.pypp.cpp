543c16a2e1fe9929a3796e9736705b354c06458d
pmtri80 <pmtri80@6a0925fc-cb2e-11dd-86c2-0f8696b7b6f9>
2010-04-30 23:30:21 +0000

    improved CvConnectedComp

--- src/pyopencv/pyopencvext/CvConnectedComp.pypp.cpp
+++ src/pyopencv/pyopencvext/CvConnectedComp.pypp.cpp
@@ -8,6 +8,10 @@
 
 namespace bp = boost::python;
 
+static cv::Scalar_<double> *get_value(CvConnectedComp const &inst) { return (cv::Scalar_<double> *)(&inst.value); }
+
+static cv::Rect_<int> *get_rect(CvConnectedComp const &inst) { return (cv::Rect_<int> *)(&inst.rect); }
+
 static ::CvSeq * get_contour( ::CvConnectedComp const & inst ) { return inst.contour; }
 
 void register_CvConnectedComp_class(){
@@ -15,8 +19,8 @@ void register_CvConnectedComp_class(){
     bp::class_< CvConnectedComp >( "CvConnectedComp" )    
         .add_property( "this", pyplus_conv::make_addressof_inst_getter< CvConnectedComp >() )    
         .def_readwrite( "area", &CvConnectedComp::area )    
-        .def_readwrite( "rect", &CvConnectedComp::rect )    
-        .def_readwrite( "value", &CvConnectedComp::value )    
+        .add_property( "value", bp::make_function(&::get_value, bp::return_internal_reference<>()) )    
+        .add_property( "rect", bp::make_function(&::get_rect, bp::return_internal_reference<>()) )    
         .add_property( "contour", bp::make_function(&::get_contour, bp::return_internal_reference<>()) );
 
 }
