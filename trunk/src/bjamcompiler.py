0f797cc608a54d3cece355db0a2375eb21610722
pmtri80 <pmtri80@6a0925fc-cb2e-11dd-86c2-0f8696b7b6f9>
2010-07-27 14:27:41 +0000

    improved CMakeLists.txt, about to branch out to modularise pyopencv

--- trunk/src/bjamcompiler.py
+++ trunk/src/bjamcompiler.py
@@ -50,7 +50,7 @@ class BjamCompiler(ccompiler.CCompiler):
         f.write('''
 import python ;
 
-using gcc :  :  g++ : <compileflags>-O3 %s <compileflags>-Wno-strict-aliasing %s
+using gcc :  :  g++ : <compileflags>-O3 <compileflags>-v %s <compileflags>-Wno-strict-aliasing %s
 ''' % (fPIC(), reloc_stdc()))
         for include_dir in objects[1]['include_dirs']:
             f.write('    <compileflags>-I%s\n' % mypath(include_dir))
