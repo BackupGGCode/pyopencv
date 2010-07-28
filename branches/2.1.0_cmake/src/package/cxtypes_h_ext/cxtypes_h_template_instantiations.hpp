a6f3599394a008cecee48498ac3fba7efbd6f105
pmtri80 <pmtri80@6a0925fc-cb2e-11dd-86c2-0f8696b7b6f9>
2010-07-28 12:23:42 +0000

    added some missing files

---
+++ branches/2.1.0_cmake/src/package/cxtypes_h_ext/cxtypes_h_template_instantiations.hpp
@@ -0,0 +1,10 @@
+#ifndef SD_cxtypes_h_TEMPLATE_INSTANTIATIONS_HPP
+#define SD_SD_cxtypes_h_TEMPLATE_INSTANTIATIONS_HPP
+
+class cxtypes_h_dummy_struct {
+public:
+    struct dummy_struct2 {};
+    static const int total_size = 0;
+};
+
+#endif
---
+++ branches/2.1.0_cmake/src/package/cxtypes_h_ext/cxtypes_h_wrapper.hpp
@@ -0,0 +1,10 @@
+#ifndef SD_CXTYPES_H_WRAPPER_H
+#define SD_CXTYPES_H_WRAPPER_H
+
+#include <cxcore.h>
+#include "cxtypes_h_template_instantiations.hpp"
+
+
+struct CvGenericHash {};
+
+#endif
